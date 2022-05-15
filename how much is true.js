88f50d72eb124c453566faf006170129a251479e
Your Name <you@example.com>
2022-05-15 21:23:20 +0700

    test.js

---
+++ how much is true.js
@@ -0,0 +1,11 @@
+function countTrue(arr) {
+	let count = 0;
+	
+	for(let i=0; i<arr.length ; i++ ){
+		if(arr[i] == true){
+			count++;
+		}
+	}
+	return count;
+}
+ console.log(countTrue([true, false, true, true]));
