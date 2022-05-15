f798765ed06902995f79299c5878a29904eb8529
Your Name <you@example.com>
2022-05-15 22:22:35 +0700

    100.0

--- Tile Teamwork Tactics.js
+++ Tile Teamwork Tactics.js
@@ -1,1 +1,15 @@
-function 
+function possibleBonus(a,b){
+    let x = a;
+    let y = b;
+
+    for(let i =0 ; i < 6; i++ ){
+        x += 1;
+        if(x==y){
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(possibleBonus(6,6));
+console.log(possibleBonus(5,6));
